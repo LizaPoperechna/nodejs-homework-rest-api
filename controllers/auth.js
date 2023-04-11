30af900bd6eb3d1cc2a8df056cca6ce32ade9fa4
LizaPoperechna <poperechnayaliza@gmail.com>
2023-04-11 01:12:12 -0400

    ДЗ 5
    аватарки

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,17 @@
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const gravatar = require('gravatar');
+const path = require('path');
+const fs = require('fs/promises');
+
 const { User } = require('../models/user');
 const { HttpError, ctrlWrapper } = require('../helpers');
+
 const {SECRET_KEY} = process.env;
 // SECRET_KEY=fghjllkvcfiolmnhc - розумію, що не потрібно сюди додавати, але, рапром він потрібен буде для перевірки ДЗ. потім видалю
 
+
+const avatarsDir = path.join(__dirname, '../', 'public', 'avatars');
 
 const register = async(req, res) => {
     const {email, password} = req.body;
@@ -15,8 +22,10 @@ const register = async(req, res) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
+    //const avatarURL = gravatar.url(email);
+    const avatarURL = gravatar.url(email, { d: 'identicon'});
 
-    const newUser = await User.create({ ...req.body, password: hashPassword });
+    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL});
 
     console.log(password);
 
@@ -25,7 +34,6 @@ const register = async(req, res) => {
 		subscription: newUser.subscription,
 	});
 }
-
 
 const login = async(req, res)=> {
     const {email, password} = req.body;
@@ -73,9 +81,24 @@ const logout = async(req, res) => {
     })
 }
 
+
+const updateAvatar = async(req, res)=> {
+    const {_id} = req.user;
+    const {path: tempUpload, originalname} = req.file;
+    const filename = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join('avatars', filename);
+    await User.findByIdAndUpdate(_id, {avatarURL});
+
+    res.json({
+        avatarURL,
+    })
+}
 module.exports = {
     register: ctrlWrapper(register),
     login: ctrlWrapper(login),
     getCurrent: ctrlWrapper(getCurrent),
     logout: ctrlWrapper(logout),
+    updateAvatar: ctrlWrapper(updateAvatar),
 }
